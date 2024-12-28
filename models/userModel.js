876a59440a9db2069df95405f6de3088a27b3183
Kevin <pintokevin2002@hotmail.com>
2024-12-28 13:52:37 +0100

    Adding a model for the user

---
+++ models/userModel.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minlength: 3,
+        maxlength: 20
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false
+    },
+});
+
+userSchema.methods.matchPassword = async function(password){
+    return await bcrypt.compare(password, this.password);
+}
+
+userSchema.pre('save', async function(next){
+    if(!this.isModified('password')) next();
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
+module.exports = mongoose.model('User', userSchema);
