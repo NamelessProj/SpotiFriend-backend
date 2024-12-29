d2a865b60f5b2a1cc0e71d16577d037b9812bbf2
Kevin <pintokevin2002@hotmail.com>
2024-12-29 20:24:28 +0100

    Updating the data store of each proposition

--- models/propositionModel.js
+++ models/propositionModel.js
@@ -1,12 +1,55 @@
 const mongoose = require('mongoose');
 
-const propositionSchema = mongoose.Schema({
-    room: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Room',
+const authorSchema = mongoose.Schema({
+    name: {
+        type: String,
         required: true
     },
-    song: {}
+    href: {
+        type: String,
+        required: true
+    }
+});
+
+const propositionSchema = mongoose.Schema({
+    room: {
+        type: String,
+        required: true
+    },
+    song: {
+        authors: {
+            type: [authorSchema],
+            required: true
+        },
+        duration: {
+            type: Number,
+            required: true
+        },
+        explicit: {
+            type: Boolean,
+            required: true
+        },
+        href: {
+            type: String,
+            required: true
+        },
+        id: {
+            type: String,
+            required: true
+        },
+        name: {
+            type: String,
+            required: true
+        },
+        isPlayable: {
+            type: Boolean,
+            required: true
+        },
+        uri: {
+            type: String,
+            required: true
+        }
+    }
 });
 
 module.exports = mongoose.model('Proposition', propositionSchema);
