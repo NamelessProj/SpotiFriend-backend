c9ad718c2601645e5fbbbb30c1a29855a0264f32
Kevin <pintokevin2002@hotmail.com>
2024-12-29 20:50:44 +0100

    Adding a method to get the access token

---
+++ controllers/accessController.js
@@ -0,0 +1,15 @@
+const asyncHandler = require('express-async-handler');
+const Access = require('../models/accessModel');
+const {getAccessToken} = require('../utils/getAccessToken');
+
+// @desc   Getting the access token
+// @route  GET /api/access/
+// @access Public
+const getTheAccessToken = asyncHandler(async (req, res) => {
+    const token = await getAccessToken();
+    res.status(200).json({token});
+});
+
+module.exports = {
+    getTheAccessToken
+}
