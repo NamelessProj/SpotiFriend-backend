060c88a76e3145efe70691b140cfe4acfe8f7d60
Kevin <pintokevin2002@hotmail.com>
2024-12-29 21:23:11 +0100

    Adding a middleware to let only admin though

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -19,6 +19,25 @@ const protect = asyncHandler(async (req, res, next) => {
         res.status(401);
         throw new Error('Not authorized, no token.');
     }
+})
+
+const adminProtect = asyncHandler(async (req, res, next) => {
+    const token = req.cookies.jwt;
+
+    if(token){
+        try{
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            req.user = await User.findOne({_id: decoded._id, role: 'admin'});
+            next();
+        }catch(error){
+            console.log(error);
+            res.status(401);
+            throw new Error('Not authorized, token error.');
+        }
+    }else{
+        res.status(401);
+        throw new Error('Not authorized, no token.');
+    }
 });
 
-module.exports = {protect}
+module.exports = {protect,adminProtect};
