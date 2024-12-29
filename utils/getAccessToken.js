d147b8c79cdf0f7c33e25c6e8467c5961ef48fb0
Kevin <pintokevin2002@hotmail.com>
2024-12-29 20:50:25 +0100

    Deleting the old access token when getting the new one

--- utils/getAccessToken.js
+++ utils/getAccessToken.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 
 const getAccessToken = async () => {
     const accessExist = await Access.findOne().sort({createdAt: -1});
-    if(accessExist) return accessExist.token;
+    if(accessExist) return accessExist;
     const response = await axios.post('https://accounts.spotify.com/api/token', {
         grant_type: 'client_credentials',
         client_id: process.env.SPOTIFY_CLIENT_ID,
@@ -11,6 +11,7 @@ const getAccessToken = async () => {
     });
     const token = response.data.access_token;
     await Access.create({token});
+    await Access.deleteMany({createdAt: {$lt: new Date(Date.now() - 3600000)}}); // Deleting the token after 1 hour
     return token;
 }
 
