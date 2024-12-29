7cbc1eb1ae7124a1d4415fad1690d4b33b86a80a
Kevin Da Silva Pinto <pintokevin2002@hotmail.com>
2024-12-29 14:02:18 +0100

    Update propositionModel.js

--- models/propositionModel.js
+++ models/propositionModel.js
@@ -3,8 +3,7 @@ const mongoose = require('mongoose');
 const propositionSchema = mongoose.Schema({
     user: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
+        ref: 'User'
     },
     room: {
         type: mongoose.Schema.Types.ObjectId,
