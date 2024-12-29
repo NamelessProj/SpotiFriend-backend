5f6378deefccd271cf9e88b1bf7f8e8d6494cd84
Kevin <pintokevin2002@hotmail.com>
2024-12-29 21:00:39 +0100

    Making sure the request for the access token works

--- utils/getAccessToken.js
+++ utils/getAccessToken.js
@@ -8,6 +8,10 @@ const getAccessToken = async () => {
         grant_type: 'client_credentials',
         client_id: process.env.SPOTIFY_CLIENT_ID,
         client_secret: process.env.SPOTIFY_CLIENT_SECRET
+    }, {
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
     });
     const token = response.data.access_token;
     await Access.create({token});
