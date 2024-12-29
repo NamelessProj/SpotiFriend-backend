4ec579419759b3450766a091f5620aab7a957adb
Kevin <pintokevin2002@hotmail.com>
2024-12-29 20:40:16 +0100

    Adding routes for the methods

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const {protect} = require('../middleware/authMiddleware');
 
 // @route User route (POST)
 // @desc Route to create a user
@@ -11,5 +12,15 @@ router.route('/login').post(userController.login);
 // @desc Route to create a user
 // @access Public
 router.route('/register').post(userController.register);
+
+// @route User route (POST)
+// @desc Route to logout a user
+// @access Public
+router.route('/logout').post(userController.logout);
+
+// @route User route (POST)
+// @desc Route to delete a user
+// @access Private
+router.route('/').delete(protect, userController.deleteUser);
 
 module.exports = router;
