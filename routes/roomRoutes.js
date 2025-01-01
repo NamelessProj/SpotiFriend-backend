823eb4d4502154943ba4035486fde60175e94150
Kevin <pintokevin2002@hotmail.com>
2025-01-01 19:06:42 +0100

    Creating all routes for the room methods

---
+++ routes/roomRoutes.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const roomController = require('../controllers/roomController');
+const {adminProtect} = require('../middleware/authMiddleware');
+
+// @route Room route (GET)
+// @desc Route to get every room
+// @access Private (admin)
+router.route('/').get(adminProtect, roomController.getRooms);
+
+// @route Room route (POST)
+// @desc Route to create a room
+// @access Private (admin)
+router.route('/').post(adminProtect, roomController.createRoom);
+
+// @route Room route (PUT)
+// @desc Route to update a room
+// @access Private (admin)
+router.route('/:id').put(adminProtect, roomController.updateRoom);
+
+// @route Room route (DELETE)
+// @desc Route to delete a room
+// @access Private (admin)
+router.route('/:id').delete(adminProtect, roomController.deleteRoom);
+
+module.exports = router;
