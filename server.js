fb24220db9cc9f06cd929f6824bb6628fa4b9d81
Kevin <pintokevin2002@hotmail.com>
2025-01-02 20:30:02 +0100

    Using cookie-parser

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const {errorHandler} = require('./middleware/errorHandler');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 const corsOptions = require('./config/corsOptions');
 const connectDB = require('./config/dbConnection');
 
@@ -17,6 +18,7 @@ connectDB();
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
 
 // ROUTES
 app.use('/api/user', require('./routes/userRoutes'));
