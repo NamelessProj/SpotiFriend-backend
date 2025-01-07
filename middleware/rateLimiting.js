de2dd93cd66fb06c1b445821104ef32bc8ddc2b4
Pinto <pintokevin2002@hotmail.com>
2025-01-07 13:15:41 +0100

    Adding a rate limit

---
+++ middleware/rateLimiting.js
@@ -0,0 +1,18 @@
+const {rateLimit} = require("express-rate-limit");
+
+const limiter = rateLimit({
+    windowMs: 60000, // 1 minute (60 * 1000)
+    limit: 100,
+    statusCode: 429,
+    standardHeaders: true,
+    legacyHeaders: false,
+    message: "Too many requests from this IP, please try again after a minute.",
+    handler: (req, res) => {
+        res.status(429).json({message: "Too many requests from this IP, please try again after a minute."});
+    },
+    keyGenerator: (req) => {
+        return req.ip;
+    }
+});
+
+module.exports = {limiter}
