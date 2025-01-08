fc0b7b13b358375932c99045216bdda5f6096ab6
Kevin <pintokevin2002@hotmail.com>
2025-01-08 22:24:59 +0100

    Updating the errorHandler

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,6 +1,6 @@
 const errorHandler = (err, req, res) => {
     const statusCode = res.statusCode ? res.statusCode : 500;
-    res.status(statusCode).send({
+    res.send({
         message: err.message,
         stack: process.env.NODE_ENV === 'prod' ? null : err.stack,
     });
