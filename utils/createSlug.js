01c016c01b630f5af29dc7131b2075956c18c282
Kevin <pintokevin2002@hotmail.com>
2025-01-11 10:03:46 +0100

    Updating the way we create slug for the rooms

--- utils/createSlug.js
+++ utils/createSlug.js
@@ -1,5 +1,13 @@
-const createSlug = (text) => {
-    return text.toString().toLowerCase().replace(/\s+/g, '-')
+const Room = require("../models/roomModel");
+
+const createSlug = async (name, id=null) => {
+    let slugExists;
+    let slug = name.toString().toLowerCase().replace(/\s+/g, '-');
+    if(id){
+        slugExists = await Room.find({slug, _id: {$ne: id}}).countDocuments();
+    }else slugExists = await Room.find({slug}).countDocuments();
+    if(slugExists > 0) slug += `-${slugExists + 1}`;
+    return slug;
 }
 
-module.exports = createSlug;
+module.exports = {createSlug};
